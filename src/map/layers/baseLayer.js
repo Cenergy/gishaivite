d76d4e6f7f7233ecc3cc2193273dfbfece4c415c
Cenergy <673598118@qq.com>
2025-06-10 00:43:23 +0800

    有没有绝佳的的和地图的 map 通信方式啊，现在的事件管理 - 需要小心处理事件的注册和注销，避免内存泄漏,希望不使用vue的相关方法，这样可以轻易的移植到别的项目上，pubsub库推荐使用吗

--- src/map/layers/baseLayer.js
+++ src/map/layers/baseLayer.js
@@ -1,91 +1,91 @@
 // 使用浏览器原生的crypto.randomUUID()替代uuid
 export default class BaseLayer {
   constructor() {
-    this.pulseIdList = [];
+    this.pulseIdList = []
   }
   init(map, isSmallMap) {
     // 假如是相同的处理类型，则注入
-    if (isSmallMap !== Boolean(this.isSmallMap)) return;
-    this.map = map;
-    this._id = crypto.randomUUID();
-    this._hasLoaded = false;
-    this.timeInterval = 10 * 1000;
+    if (isSmallMap !== Boolean(this.isSmallMap)) return
+    this.map = map
+    this._id = crypto.randomUUID()
+    this._hasLoaded = false
+    this.timeInterval = 10 * 1000
   }
   get id() {
-    return this._id;
+    return this._id
   }
   get hasLoaded() {
-    return this._hasLoaded;
+    return this._hasLoaded
   }
   set hasLoaded(status) {
-    this._hasLoaded = Boolean(status);
+    this._hasLoaded = Boolean(status)
   }
 
-  openPointPopup({ content = "", component, latlng, options = {} }) {
+  openPointPopup({ content = '', component, latlng, options = {} }) {
     // TODO: 使用MapTalks的UI组件实现弹窗
     // MapTalks使用InfoWindow或自定义UI组件
-    console.log('openPointPopup called with:', { content, latlng, options });
+    console.log('openPointPopup called with:', { content, latlng, options })
   }
   closePointPopup() {
-    this.popup && this.popup.close();
-    this.popup = null;
+    this.popup && this.popup.close()
+    this.popup = null
   }
 
   setVisible(visible, options = {}) {
     if (visible) {
-      this._debounce(this.show.bind(this), 200)(options);
+      this._debounce(this.show.bind(this), 200)(options)
     } else {
-      this._debounce(this.hide.bind(this), 200)(options);
+      this._debounce(this.hide.bind(this), 200)(options)
     }
   }
 
   show(options) {
-    this._showCore(options);
+    this._showCore(options)
   }
   _showCore(options) {
-    this._throwNotImplementationError();
+    this._throwNotImplementationError()
   }
 
   hide(options) {
-    this._visible = false;
-    this._hideCore(options);
+    this._visible = false
+    this._hideCore(options)
   }
 
   _hideCore(options) {
-    this._throwNotImplementationError();
+    this._throwNotImplementationError()
   }
 
   setLegend(visible) {
-    if (!this.legend) return;
-    this._setLegend(visible);
+    if (!this.legend) return
+    this._setLegend(visible)
   }
 
-  hidePulse(id){
+  hidePulse(id) {
     // TODO: 使用MapTalks实现脉冲效果的隐藏
-    console.log('hidePulse called with id:', id);
+    console.log('hidePulse called with id:', id)
   }
-  showPulse(id){
+  showPulse(id) {
     // TODO: 使用MapTalks实现脉冲效果的显示
-    console.log('showPulse called with id:', id);
+    console.log('showPulse called with id:', id)
   }
 
   _setLegend(visible) {
-    this._throwNotImplementationError();
+    this._throwNotImplementationError()
   }
 
   _handlePublishHour(post_modified) {
     // 拿到当前时间戳和发布时的时间戳，然后得出时间戳差
-    const curTime = new Date();
-    const postTime = new Date(post_modified);
-    const timeDiff = curTime.getTime() - postTime.getTime();
+    const curTime = new Date()
+    const postTime = new Date(post_modified)
+    const timeDiff = curTime.getTime() - postTime.getTime()
 
     // 单位换算
-    const min = 60 * 1000;
-    const hour = min * 60;
+    const min = 60 * 1000
+    const hour = min * 60
 
     // 计算发布时间距离当前时间的小时
-    const exceedHour = Math.floor(timeDiff / hour);
-    return exceedHour;
+    const exceedHour = Math.floor(timeDiff / hour)
+    return exceedHour
   }
 
   /**
@@ -96,18 +96,18 @@ export default class BaseLayer {
    */
   _debounce(fn, delay) {
     // 所以这个函数就可以使用...运算符收集js自动添加的参数到一个数组中
-    const { name: timerId = "toggleShowLayerTimer" } = this.constructor;
+    const { name: timerId = 'toggleShowLayerTimer' } = this.constructor
 
     return (...arg) => {
-      if (this[timerId]) clearTimeout(this[timerId]);
+      if (this[timerId]) clearTimeout(this[timerId])
       this[timerId] = setTimeout(() => {
         // 通过apply绑定this和传递参数，apply第二个参数正好是传数组嘛
-        fn.apply(this, arg);
-      }, delay);
-    };
+        fn.apply(this, arg)
+      }, delay)
+    }
   }
 
   _throwNotImplementationError() {
-    throw new Error("method not implementation.");
+    throw new Error('method not implementation.')
   }
 }
