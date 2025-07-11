2b2f26db59f70056e20ddb0991a6fc0ae3de99e3
cenergy <673598118@qq.com>
2025-07-11 15:42:59 +0800

    perf(map): 使用异步组件优化地图组件加载性能
    将地图相关组件改为异步加载方式，减少初始加载时的资源消耗

--- src/components/map/index.ts
+++ src/components/map/index.ts
@@ -1,4 +1,9 @@
-export { default as LayerControl } from './LayerControl.vue'
-export { default as MapDetail } from './MapDetail.vue'
-export { default as MapToolbar } from './MapToolbar.vue'
+import { defineAsyncComponent } from 'vue';
+
+// 懒加载组件
+export const LayerControl = defineAsyncComponent(() => import('./LayerControl.vue'));
+export const MapDetail = defineAsyncComponent(() => import('./MapDetail.vue'));
+export const MapToolbar = defineAsyncComponent(() => import('./MapToolbar.vue'));
+
+// 导出 popups 模块
 export * from './popups';
